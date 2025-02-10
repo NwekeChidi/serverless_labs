c9237cd7ad64273bb7fed057496db3f206c44d79
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2025-02-10 12:30:03 +0000

    -- cronJobLambda

--- cronJobLambda/src/index.js
+++ cronJobLambda/src/index.js
@@ -1,24 +1,23 @@
-module.exports.handler = async event => {
-  console.log(`Lambda function triggered by EventBridge Scheduler on: ${new Date()}`)
+module.exports.handler = async (event) => {
+  console.log(`Lambda function triggered by EventBridge Scheduler on: ${new Date()}`);
 
   try {
-    const response = await downstreamProcess()
-    console.log(`Downstream process completed with response: ${response}`)
-  }
-  catch (error) {
-    console.log(`An Error Occurred 😕: ${error}`)
-    throw
+    const response = await downstreamProcess();
+    console.log(`Downstream process completed with response: ${response}`);
+  } catch (error) {
+    console.log(`An Error Occurred 😕: ${error}`);
+    throw new Error("LambdaException");
   }
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Task Completed!🎉"
-    })
-  }
-}
+      message: "Task Completed!🎉",
+    }),
+  };
+};
 
 const downstreamProcess = async () => {
   // Function to simulate a downstream process
-  return `A New Hope on ${new Date()}!`
-}
+  return `A New Hope on ${new Date()}!`;
+};
