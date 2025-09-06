15e999a07b53183f229ecb255d9778da3cb7e0bb
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2025-09-06 22:43:03 +0100

    🎉 Initial Commit

---
+++ eda-mcp/test/eda-mcp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EdaMcp from '../lib/eda-mcp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eda-mcp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EdaMcp.EdaMcpStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
